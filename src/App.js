62f367cc0f1774bec194191b232761a2b86dd758
Lulama Cele <lulamac32@gmail.com>
2024-03-26 16:34:08 +0200

    made changes to try fix routes, issue page not displaying when deployed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import './App.css';
-import {Nav} from './components/nav/Nav';
-import {Home} from './pages/home/Home';
-import {Services} from './pages/services/Services';
-import { Route, Routes } from 'react-router-dom';
-
-
+import React from "react";
+import "./App.css";
+import { Nav } from "./components/nav/Nav";
+import { Home } from "./pages/home/Home";
+import { Services } from "./pages/services/Services";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     {/* <Home /> */}
-     <Routes>
-     <Route path='' element={<Home />}/>
-     <Route path='/services' element={<Services />} />
-     </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" >
+            <Home />
+          </Route>
+          <Route path="/services" element={<Services />} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
