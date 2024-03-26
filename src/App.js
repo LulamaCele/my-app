ef554ce7b9d62729ea940ba4a803953c812baea4
Lulama Cele <lulamac32@gmail.com>
2024-03-26 14:36:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          THIS IS A TESTING APP FOR GH-PAGES
         </p>
         <a
           className="App-link"
